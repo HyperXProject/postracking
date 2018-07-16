b77fdbc0ebbae6a8568b07670e3e13c7d6f76802
HyperXProject <HyperXProject@users.noreply.github.com>
2018-07-16 00:33:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if it's not present, don't show loader */
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
 <script>
-	
+	//paste this code under head tag or in a seperate js file.
 	// Wait for window load
 	$(window).load(function() {
 		// Animate loader off screen
