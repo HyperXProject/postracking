054ef85cd6a6043f453556c838cc59e1626f5123
HyperXProject <HyperXProject@users.noreply.github.com>
2018-07-16 00:44:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -25,42 +25,10 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	
 <!--===============================================================================================-->
-<style>
-/* Paste this css to your style sheet file or under head tag */
-/* This only works with JavaScript, 
-if it's not present, don't show loader */
-.no-js #loader { display: none;  }
-.js #loader { display: block; position: absolute; left: 100px; top: 0; }
-.se-pre-con {
-	position: fixed;
-	left: 0px;
-	top: 0px;
-	width: 100%;
-	height: 100%;
-	z-index: 9999;
-	background: url(images/loading/Preloader.gif) center no-repeat #fff;
-}
-</style>
-	
-	
-<!-- Loading Animation -->	
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
-<script>
-	//paste this code under head tag or in a seperate js file.
-	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
-</script>
-<!-- Loading Animation -->	
+
 
 </head>
-<body>
-
-    <!-- Loading Animation -->
-    <div class="se-pre-con">
+<body> 
 	
 	
 	<div class="container-contact100">
@@ -107,7 +75,7 @@ if it's not present, don't show loader */
                                 <center><span class="label-input100">© 2018 - JomDevelop</span></center>
 			</form>
 		</div>
-	</div></div>
+	</div>
 
 
 
